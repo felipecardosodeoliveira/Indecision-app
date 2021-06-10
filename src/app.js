662f7a52dca7a06817ee8de8409c310c0c504775
lipeoliveira <felipecardosodeoliveira02@gmail.com>
2021-06-09 23:35:45 -0300

    install react and react-dom

--- src/app.js
+++ src/app.js
@@ -1,21 +1,5 @@
-// import subtract, { square, add } from './utils';
+import React from 'react';
+import { render } from 'react-dom';
 
-// console.log('app.js is running now')
-
-// console.log(square(2));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81))
-
-// person.js
-// named export isAdult(18) - true if adult, otherwise false
-// named export canDrink(21) - true if 21 and over, otherwise false
-
-// import isAdult and canDrink
-// use both printing result to the console
-
-import isSenior, { isAdult, canDrink } from './playground/person';
-
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(isSenior(64));
-console.log(isSenior(65));
+const template = React.createElement('p', {}, 'React Element');
+render(template, document.getElementById('app'))
